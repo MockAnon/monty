d554a35130961af2efb6113506247fe64061eda6
Jaron Evans <jarondevans@gmail.com>
2018-07-08 00:12:57 +0000

    update

--- https_example.js
+++ https_example.js
@@ -12,6 +12,12 @@ var callback = function() {
   console.log('In response handler callback!');
 }
 
+// called by https when the request is made.
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Response: ', response);
+}
+
 console.log("I'm about to make the request!");
 
 https.request(options, callback).end();
